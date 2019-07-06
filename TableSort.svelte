72409daf6ace64d69ea29cfd1fd71575d70fd2fb
Mattias Holmlund <mattias@holmlund.se>
2019-07-06 16:21:06 +0200

    Format with prettier

--- TableSort.svelte
+++ TableSort.svelte
@@ -5,10 +5,14 @@
     const CLASSNAME_DESC = 'descending'
 
     import { onMount } from 'svelte'
-    import {sortGenerator, compareStrings, compareNumbers} from './src/sort.js'
+    import {
+        sortGenerator,
+        compareStrings,
+        compareNumbers,
+    } from './src/sort.js'
     export let items
-    let className=""
-    export {className as class}
+    let className = ''
+    export { className as class }
 
     let thead
     let sortOrder = [[]]
@@ -16,65 +20,72 @@
     $: sortedItems = sorted([...items], sortOrder)
 
     const sorted = function(arr, sortOrder) {
-        arr.sort(sortGenerator( function*(a,b) {
-            for( let [fieldName, r] of sortOrder) {
-                const reverse = r===0 ? 1 : -1
-                if(typeof a[fieldName] === 'number') {
-                    yield reverse*compareNumbers(a[fieldName], b[fieldName])  
+        arr.sort(
+            sortGenerator(function*(a, b) {
+                for (let [fieldName, r] of sortOrder) {
+                    const reverse = r === 0 ? 1 : -1
+                    if (typeof a[fieldName] === 'number') {
+                        yield reverse *
+                            compareNumbers(a[fieldName], b[fieldName])
+                    } else {
+                        yield reverse *
+                            compareStrings(a[fieldName], b[fieldName])
+                    }
                 }
-                else {
-                    yield reverse*compareStrings(a[fieldName], b[fieldName])  
-                }
-            }
-
-        } ))
+            })
+        )
 
         return arr
     }
 
     function updateSortOrder(th, push) {
         const fieldName = th.dataset.sort
-        if(push) {
-            if(sortOrder[sortOrder.length-1][0] === fieldName) {
-                sortOrder[sortOrder.length-1] = [fieldName, (sortOrder[sortOrder.length-1][1] + 1) % 2]
-            }
-            else {
+        if (push) {
+            if (sortOrder[sortOrder.length - 1][0] === fieldName) {
+                sortOrder[sortOrder.length - 1] = [
+                    fieldName,
+                    (sortOrder[sortOrder.length - 1][1] + 1) % 2,
+                ]
+            } else {
                 sortOrder = [...sortOrder, [fieldName, 0]]
             }
-        }
-        else {
-            if(sortOrder.length === 1 && sortOrder[0][0] === fieldName) {
+        } else {
+            if (sortOrder.length === 1 && sortOrder[0][0] === fieldName) {
                 sortOrder[0] = [fieldName, (sortOrder[0][1] + 1) % 2]
-            }
-            else {
+            } else {
                 resetClasses()
                 sortOrder = [[fieldName, 0]]
             }
         }
-        th.className = CLASSNAME_SORTABLE + ' ' + (sortOrder[sortOrder.length-1][1] ? CLASSNAME_DESC : CLASSNAME_ASC)
+        th.className =
+            CLASSNAME_SORTABLE +
+            ' ' +
+            (sortOrder[sortOrder.length - 1][1]
+                ? CLASSNAME_DESC
+                : CLASSNAME_ASC)
     }
 
     function resetClasses() {
         const th = thead.getElementsByTagName('th')
-        for(let i = 0; i < th.length; i++) {
-            if(th[i].dataset.sort) {
-                th[i].className=CLASSNAME_SORTABLE
+        for (let i = 0; i < th.length; i++) {
+            if (th[i].dataset.sort) {
+                th[i].className = CLASSNAME_SORTABLE
             }
         }
     }
 
-    onMount( () => {
+    onMount(() => {
         const th = thead.getElementsByTagName('th')
-        for(let i = 0; i < th.length; i++) {
-            if(th[i].dataset.sort) {
-                th[i].className=CLASSNAME_SORTABLE
-                th[i].onclick = (event) => updateSortOrder(th[i],event.shiftKey)
+        for (let i = 0; i < th.length; i++) {
+            if (th[i].dataset.sort) {
+                th[i].className = CLASSNAME_SORTABLE
+                th[i].onclick = (event) =>
+                    updateSortOrder(th[i], event.shiftKey)
             }
-            if(th[i].dataset.sortInitial === "descending") {
+            if (th[i].dataset.sortInitial === 'descending') {
                 th[i].className = CLASSNAME_SORTABLE + ' ' + CLASSNAME_DESC
                 sortOrder = [...sortOrder, [th[i].dataset.sort, 1]]
-            }
-            else if(th[i].dataset.sortInitial != undefined) {
+            } else if (th[i].dataset.sortInitial != undefined) {
                 th[i].className = CLASSNAME_SORTABLE + ' ' + CLASSNAME_ASC
                 sortOrder = [...sortOrder, [th[i].dataset.sort, 0]]
             }
@@ -83,26 +94,25 @@
 </script>
 
 <style>
-thead :global(th.sortable) {
-	cursor: pointer;
-	user-select: none;
-	-moz-user-select: none;
-	-webkit-user-select: none;
-	-ms-user-select: none;
-}
+    thead :global(th.sortable) {
+        cursor: pointer;
+        user-select: none;
+        -moz-user-select: none;
+        -webkit-user-select: none;
+        -ms-user-select: none;
+    }
 </style>
 
 <table class="{CLASSNAME_TABLE} {className}">
-	<thead bind:this={thead}>
-		<slot name="thead">
-		</slot>
-	</thead>
-	<tbody>
-	{#each sortedItems as item}
-        <slot name="tbody" item={item}></slot>
-	{/each}
-	</tbody>
+    <thead bind:this={thead}>
+        <slot name="thead" />
+    </thead>
+    <tbody>
+        {#each sortedItems as item}
+            <slot name="tbody" {item} />
+        {/each}
+    </tbody>
     <tfoot>
-        <slot name="tfoot"></slot>
+        <slot name="tfoot" />
     </tfoot>
 </table>
